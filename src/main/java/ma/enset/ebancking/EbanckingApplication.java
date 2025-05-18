6bb4dc9a41afc7896fe8aa6ef08d34be7c964189
oussamabartil <oussamabartil.04@gmail.com>
2025-05-18 13:43:17 +0000

    initialisation des clients et comptes bancaires de test

--- src/main/java/ma/enset/ebancking/EbanckingApplication.java
+++ src/main/java/ma/enset/ebancking/EbanckingApplication.java
@@ -1,6 +1,9 @@
 package ma.enset.ebancking;
 
+import ma.enset.ebancking.entities.CurrentAccount;
 import ma.enset.ebancking.entities.Customer;
+import ma.enset.ebancking.entities.SavingAccount;
+import ma.enset.ebancking.enumes.AccountStatus;
 import ma.enset.ebancking.repositories.AccountOperationRepository;
 import ma.enset.ebancking.repositories.BankAccountRepository;
 import ma.enset.ebancking.repositories.CustomerRepository;
@@ -9,6 +12,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Date;
+import java.util.UUID;
 import java.util.stream.Stream;
 
 @SpringBootApplication
@@ -29,6 +34,30 @@ public class EbanckingApplication {
                 customer.setEmail(name+"@gmail.com");
                 customerRepository.save(customer);
             });
+
+            customerRepository.findAll().forEach(cust->{
+                CurrentAccount currentAccount = new CurrentAccount();
+                currentAccount.setId(UUID.randomUUID().toString());
+                currentAccount.setBalance(Math.random()*90000);
+                currentAccount.setCreatedAt(new Date());
+                currentAccount.setStatus(AccountStatus.CREATED);
+                currentAccount.setCustomer(cust);
+                currentAccount.setOverDraft(9000);
+                bankAccountRepository.save(currentAccount);
+
+                SavingAccount savingAccount = new SavingAccount();
+                savingAccount.setId(UUID.randomUUID().toString());
+                savingAccount.setBalance(Math.random()*90000);
+                savingAccount.setCreatedAt(new Date());
+                savingAccount.setStatus(AccountStatus.CREATED);
+                savingAccount.setCustomer(cust);
+                savingAccount.setInterestRate(5.5);
+                bankAccountRepository.save(savingAccount);
+
+
+            });
         };
     }
+
+
 }
